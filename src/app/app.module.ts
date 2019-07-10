f4166b6bddab22b8c59e30c479d954c0ef4f1886
dbialecki <dbialecki@consdata.pl>
2019-07-10 23:13:06 +0200

    Style changes - routed components take 100% of available space now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule {
+}
