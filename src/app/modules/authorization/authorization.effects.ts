559bc7c587818072d52d7dbaae5ff1ac45903ba3
dbialecki <dbialecki@consdata.pl>
2019-08-11 18:38:29 +0200

    DIET-14 catchApiError util. Catches error and returns observable with action created by provided actionCreator.

--- src/app/modules/authorization/authorization.effects.ts
+++ src/app/modules/authorization/authorization.effects.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 
-import { catchError, map, mergeMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 import * as AuthorizationActions from './authorization.actions';
-import { ApiError } from '../../api/models/api-error.model';
 import { AuthorizationService } from '../../api/services/authorization.service';
+import { NgrxUtils } from '../shared/utils/ngrx-utils';
 
 
 @Injectable()
@@ -16,15 +15,16 @@ export class AuthorizationEffects {
     ofType(AuthorizationActions.signUp),
     mergeMap(({ username, password }) => this.authorizationService.signUp({ username, password }).pipe(
       map((response) => AuthorizationActions.signUpSuccess({ authorizationToken: response.token })),
-      catchError((apiError: ApiError) => of(AuthorizationActions.signUpError(apiError)))
+      NgrxUtils.catchApiError(AuthorizationActions.signUpError)
     ))
-  ));
+    )
+  );
 
   signIn$ = createEffect(() => this.actions$.pipe(
     ofType(AuthorizationActions.signIn),
     mergeMap(({ username, password }) => this.authorizationService.signIn({ username, password }).pipe(
       map((response) => AuthorizationActions.signInSuccess({ authorizationToken: response.token })),
-      catchError((apiError: ApiError) => of(AuthorizationActions.signInError(apiError)))
+      NgrxUtils.catchApiError(AuthorizationActions.signInError)
     ))
   ));
 
@@ -32,7 +32,7 @@ export class AuthorizationEffects {
     ofType(AuthorizationActions.signOut),
     mergeMap(() => this.authorizationService.signOut().pipe(
       map(() => AuthorizationActions.signOutSuccess()),
-      catchError((apiError: ApiError) => of(AuthorizationActions.signOutError(apiError)))
+      NgrxUtils.catchApiError(AuthorizationActions.signOutError)
     ))
   ));
 
--- src/app/modules/shared/utils/ngrx-utils.ts
+++ src/app/modules/shared/utils/ngrx-utils.ts
@@ -1,11 +1,14 @@
-import { createAction, props, Store } from '@ngrx/store';
-import { ApiError } from '../../../api/models/api-error.model';
-import { BehaviorSubject, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Action, ActionCreator, createAction, props, Store } from '@ngrx/store';
+import { BehaviorSubject, of, OperatorFunction, Subject } from 'rxjs';
+import { catchError, takeUntil } from 'rxjs/operators';
 import { OnDestroy } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { TypedAction } from '@ngrx/store/src/models';
 
 export class NgrxUtils {
-  static readonly createApiErrorAction = (type: string) => createAction(type, props<ApiError>());
+  static createApiErrorAction(type: string): ApiErrorActionCreator {
+    return createAction(type, props<{ error: HttpErrorResponse }>());
+  }
 
   static selectAsBehaviourSubject<T>(store: Store<unknown>, subscriber: OnDestroy, ...selectors: any[]): BehaviorSubject<T | undefined> {
     const onDestroy$ = new Subject<void>();
@@ -22,4 +25,11 @@ export class NgrxUtils {
       .subscribe((selectedState) => bs.next(selectedState));
     return bs;
   }
+
+  static catchApiError(apiErrorActionCreator: ApiErrorActionCreator): OperatorFunction<Action, Action> {
+    return catchError((error: HttpErrorResponse) => of(apiErrorActionCreator({ error })));
+  }
 }
+
+// tslint:disable-next-line:max-line-length
+type ApiErrorActionCreator = ActionCreator<string, (props: { error: HttpErrorResponse }) => { error: HttpErrorResponse } & TypedAction<string>>;
