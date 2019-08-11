86677a7681f5c93899e9b64d9de711ae11bd4830
dbialecki <dbialecki@consdata.pl>
2019-08-11 15:52:50 +0200

    DIET-14 created NgrxUtils.selectAsBehaviourSubject util

--- src/app/api/http-interceptors/authorization-token-http-interceptor.ts
+++ src/app/api/http-interceptors/authorization-token-http-interceptor.ts
@@ -1,34 +1,29 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable, Subject } from 'rxjs';
-import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { AppState } from '../../app.recuder';
 import { Store } from '@ngrx/store';
 import * as fromAuthorization from '../../modules/authorization/authorization.reducer';
-import { takeUntil } from 'rxjs/operators';
+import { OnDestroyAbstract } from '../../modules/shared/utils/abstract-injectables/on-destroy-abstract';
+import { NgrxUtils } from '../../modules/shared/utils/ngrx-utils';
 
 
 @Injectable()
-export class AuthorizationTokenHttpInterceptor implements HttpInterceptor, OnDestroy {
+export class AuthorizationTokenHttpInterceptor extends OnDestroyAbstract implements HttpInterceptor {
 
-  private onDestroy$ = new Subject<void>();
-  private authToken: string | undefined;
+  private readonly authToken$: BehaviorSubject<string | undefined>;
 
   constructor(store: Store<AppState>) {
-    store.select(fromAuthorization.selectAuthorizationToken)
-      .pipe(takeUntil(this.onDestroy$))
-      .subscribe((authToken: string | undefined) => this.authToken = authToken);
+    super();
+    this.authToken$ = NgrxUtils.selectAsBehaviourSubject(store, this, fromAuthorization.selectAuthorizationToken);
   }
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (this.authToken !== undefined) {
-      const authorizedRequest = req.clone({ setHeaders: { Authorization: this.authToken } });
+    const authToken = this.authToken$.getValue();
+    if (authToken !== undefined) {
+      const authorizedRequest = req.clone({ setHeaders: { Authorization: authToken } });
       return next.handle(authorizedRequest);
     }
     return next.handle(req);
-  }
-
-  ngOnDestroy(): void {
-    this.onDestroy$.next();
-    this.onDestroy$.complete();
   }
 }
--- src/app/modules/shared/utils/ngrx-utils.ts
+++ src/app/modules/shared/utils/ngrx-utils.ts
@@ -1,6 +1,25 @@
-import { createAction, props } from '@ngrx/store';
+import { createAction, props, Store } from '@ngrx/store';
 import { ApiError } from '../../../api/models/api-error.model';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { OnDestroy } from '@angular/core';
 
 export class NgrxUtils {
   static readonly createApiErrorAction = (type: string) => createAction(type, props<ApiError>());
+
+  static selectAsBehaviourSubject<T>(store: Store<unknown>, subscriber: OnDestroy, ...selectors: any[]): BehaviorSubject<T | undefined> {
+    const onDestroy$ = new Subject<void>();
+    const currentOnDestroy = subscriber.ngOnDestroy;
+    subscriber.ngOnDestroy = () => {
+      onDestroy$.next();
+      onDestroy$.complete();
+      currentOnDestroy();
+    };
+
+    const bs = new BehaviorSubject<T | undefined>(undefined);
+    store.select(...selectors)
+      .pipe(takeUntil(onDestroy$))
+      .subscribe((selectedState) => bs.next(selectedState));
+    return bs;
+  }
 }
