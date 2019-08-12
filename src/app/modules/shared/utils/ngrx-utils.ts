c1dce6772c1b72423db1d3af4d1bb83a54dc55ae
dbialecki <dbialecki@consdata.pl>
2019-08-12 21:58:32 +0200

    DIET-14 selectFirst ngrx util

--- src/app/modules/shared/utils/ngrx-utils.ts
+++ src/app/modules/shared/utils/ngrx-utils.ts
@@ -1,8 +1,11 @@
-import { Action, ActionCreator, createAction, props } from '@ngrx/store';
+import { Action, ActionCreator, createAction, props, Store } from '@ngrx/store';
 import { of, OperatorFunction } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, first } from 'rxjs/operators';
 import { HttpErrorResponse } from '@angular/common/http';
 import { TypedAction } from '@ngrx/store/src/models';
+
+// tslint:disable-next-line:max-line-length
+type ApiErrorActionCreator = ActionCreator<string, (props: { error: HttpErrorResponse }) => { error: HttpErrorResponse } & TypedAction<string>>;
 
 export function createApiErrorAction(type: string): ApiErrorActionCreator {
   return createAction(type, props<{ error: HttpErrorResponse }>());
@@ -12,5 +15,6 @@ export function catchApiError(apiErrorActionCreator: ApiErrorActionCreator): Ope
   return catchError((error: HttpErrorResponse) => of(apiErrorActionCreator({ error })));
 }
 
-// tslint:disable-next-line:max-line-length
-type ApiErrorActionCreator = ActionCreator<string, (props: { error: HttpErrorResponse }) => { error: HttpErrorResponse } & TypedAction<string>>;
+export function selectFirst<T, K>(store: Store<T>, selector: (t: T) => K) {
+  return store.select<K>(selector).pipe(first());
+}
