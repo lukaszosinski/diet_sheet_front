5ae5939ba00cfe0cf8009701a11996acf9376954
dominikbialecki <dominikbilecki1@gmail.com>
2019-08-16 22:56:18 +0200

    DIET-24 changed typings in api.effects

--- src/app/api/api.actions.ts
+++ src/app/api/api.actions.ts
@@ -2,12 +2,14 @@ import { Action, createAction } from '@ngrx/store';
 import { HttpErrorResponse } from '@angular/common/http';
 import { of, OperatorFunction } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { TypedAction } from '@ngrx/store/src/models';
 
 // tslint:disable-next-line:typedef
 export function createApiErrorAction(type: string, errorMessageKey?: string) {
   return createAction(type, (error: HttpErrorResponse) => ({ error, errorMessageKey }));
 }
 
+export type ApiAction = TypedAction<string> & { error: HttpErrorResponse, errorMessageKey?: string };
 export type ApiErrorActionCreator = ReturnType<typeof createApiErrorAction>;
 
 export function catchApiError(apiErrorActionCreator: ApiErrorActionCreator): OperatorFunction<Action, Action> {
