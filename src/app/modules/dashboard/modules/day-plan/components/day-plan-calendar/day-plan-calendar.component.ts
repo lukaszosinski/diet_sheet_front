312b8a640d2e2174212c6dab70f19f5b3f42e8b0
dominikbialecki <dominikbilecki1@gmail.com>
2019-08-19 20:46:05 +0200

    DIET-15 temp stylings

--- src/app/modules/dashboard/modules/day-plan/components/day-plan-calendar/day-plan-calendar.component.ts
+++ src/app/modules/dashboard/modules/day-plan/components/day-plan-calendar/day-plan-calendar.component.ts
@@ -3,9 +3,20 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 @Component({
   selector: 'diet-day-plan-calendar',
   template: `
-      <p>
-          day-plan-calendar works!
-      </p>
+      <div class="calendar">
+          <button type="button" class="calendar-redirect-button">KALENDARZ</button>
+          <ul class="calendar-list">
+              <li class="calendar-list-item">5</li>
+              <li class="calendar-list-item">6</li>
+              <li class="calendar-list-item calendar-list-item-central">
+                  <div class="calendar-list-item-central-month">Czerwiec</div>
+                  <div class="calendar-list-item-central-day">7</div>
+                  <div class="calendar-list-item-central-day-of-month">środa</div>
+              </li>
+              <li class="calendar-list-item">8</li>
+              <li class="calendar-list-item">9</li>
+          </ul>
+      </div>
   `,
   styleUrls: [ './day-plan-calendar.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
