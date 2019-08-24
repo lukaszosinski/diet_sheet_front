a155d818442b5115a7f1a8b643528d6997904faa
Łukasz Osiński <nuras9988@gmail.com>
2019-08-24 22:27:49 +0200

    DIET-16 Meal summary icons and styles.

--- src/app/modules/dashboard/modules/day-plan/components/day-plan-meal/summary/summary.component.ts
+++ src/app/modules/dashboard/modules/day-plan/components/day-plan-meal/summary/summary.component.ts
@@ -4,22 +4,27 @@ import {Summary} from "../../../../../../../api/models/summary";
 @Component({
   selector: 'diet-summary',
   template: `
-    <div class="content-wrapper">
-        <label class="summary">
-            {{summary.kcal}}
-        </label>
-        <label class="summary">
-            {{summary.carbs}}
-        </label>
-        <label class="summary">
-            {{summary.proteins}}
-        </label>
-        <label class="summary">
-            {{summary.fat}}
-        </label>
-        <label class="summary">
-            {{summary.roughage}}
-        </label>
+    <div class="summary-wrapper">
+        <div class="summary" title="{{'SUMMARY.KCAL' | translate}}">
+            <div class="summary-label">{{summary.kcal}}</div>
+            <div class="label-img kcal-img"></div>
+        </div>
+        <div class="summary" title="{{'SUMMARY.CARBS' | translate}}">
+            <div class="summary-label">{{summary.carbs}}</div>
+            <div class="label-img carbs-img"></div>
+        </div>
+        <div class="summary" title="{{'SUMMARY.PROTEINS' | translate}}">
+            <div class="summary-label">{{summary.proteins}}</div>
+            <div class="label-img proteins-img"></div>
+        </div>
+        <div class="summary" title="{{'SUMMARY.FAT' | translate}}">
+            <div class="summary-label">{{summary.fat}}</div>
+            <div class="label-img fat-img"></div>
+        </div>
+        <div class="summary" title="{{'SUMMARY.ROUGHAGE' | translate}}">
+            <div class="summary-label">{{summary.roughage}}</div>
+            <div class="label-img roughage-img"></div>
+        </div>
     </div>
 
   `,
