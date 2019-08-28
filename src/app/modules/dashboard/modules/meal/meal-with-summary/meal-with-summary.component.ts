534c96b00c3548a3d51035d0546e0b0b098756b4
dominikbialecki <dominikbilecki1@gmail.com>
2019-08-28 21:14:07 +0200

    DIET-31 meal can be selected by keyboard

--- src/app/modules/dashboard/modules/meal/meal-with-summary/meal-with-summary.component.ts
+++ src/app/modules/dashboard/modules/meal/meal-with-summary/meal-with-summary.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Meal } from '../meal.model';
 
 @Component({
   selector: 'diet-meal-with-summary',
   template: `
-      <div class="meal-content">
+      <div class="meal-content" tabindex="0" (keyup.space)="onClick($event)">
           <div class="meal-content-name">{{meal.name}}</div>
           <diet-summary [summary]="meal.summary" [wrap]="true" [theme]="'alternative'"></diet-summary>
       </div>
@@ -14,4 +14,9 @@ import { Meal } from '../meal.model';
 })
 export class MealWithSummaryComponent {
   @Input() meal!: Meal;
+  @Output() click: EventEmitter<Event> = new EventEmitter<Event>();
+
+  onClick(event: Event): void {
+    this.click.emit(event);
+  }
 }
