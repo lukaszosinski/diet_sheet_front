f996cc0f101e4466a145ce761195c2aec9a900c0
Łukasz Osiński <nuras9988@gmail.com>
2019-08-30 22:15:57 +0200

    DIET-29 FormGroup added.

--- src/app/modules/dashboard/modules/product/product-summary/product-summary.component.ts
+++ src/app/modules/dashboard/modules/product/product-summary/product-summary.component.ts
@@ -1,23 +1,23 @@
-import {Component, OnInit, ChangeDetectionStrategy, Input} from '@angular/core';
-import {Summary} from '../../../../../api/models/summary';
+import {Component, ChangeDetectionStrategy, Input} from '@angular/core';
+
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'diet-product-summary',
   template: `
-    <div class="product-summary-wrapper">
-        <input type="text" [(ngModel)]="summary.kcal">
-        <input type="text" [(ngModel)]="summary.proteins">
-        <input type="text" [(ngModel)]="summary.carbs">
-        <input type="text" [(ngModel)]="summary.fat">
+    <div class="product-summary-wrapper" [formGroup]="parentForm">
+        <div formGroupName="summary">
+            <input type="text" id="kcal" name="kcal" [formControlName]="'kcal'">
+            <input type="text" id="proteins" name="proteins" [formControlName]="'proteins'">
+            <input type="text" id="carbs" name="carbs" [formControlName]="'carbs'">
+            <input type="text" id="fat" name="fat" [formControlName]="'fat'">
+        </div>
     </div>
   `,
   styleUrls: ['./product-summary.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductSummaryComponent implements OnInit {
-  @Input() summary?: Summary;
+export class ProductSummaryComponent {
+  @Input() parentForm?: FormGroup;
   constructor() { }
-  ngOnInit() {
-  }
-
 }
--- src/app/modules/dashboard/modules/product/product/product.component.ts
+++ src/app/modules/dashboard/modules/product/product/product.component.ts
@@ -1,8 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {getEmptyProduct, Product} from '../product.model';
+import {Product} from '../product.model';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../../../../app.recuder';
 import * as ProductActions from '../product.actions';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'diet-product',
@@ -12,27 +14,37 @@ import * as ProductActions from '../product.actions';
               <button (click)="createProduct()"><span>+</span></button>
               <button><span>x</span></button>
           </div>
-          <label>
-              <input type="text" [(ngModel)]="product.name"/>
-          </label>
-          <diet-product-summary [summary]="product.summary"></diet-product-summary>
+          <form [formGroup]="form">
+              <input type="text" id="name" name="name" [formControlName]="'name'">
+              <diet-product-summary [parentForm]="form"></diet-product-summary>
+          </form>
       </div>
   `,
   styleUrls: [ './product.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent {
+  form: FormGroup = this.fb.group({});
 
-  product: Product;
+  constructor(private store: Store<AppState>,
+              private fb: FormBuilder) {
+    this.initializeForm();
+  }
 
-  constructor(private store: Store<AppState>) {
-    this.product = getEmptyProduct();
+  private initializeForm(): void {
+    this.form = this.fb.group({
+      name: 'Nowy produkt',
+      summary: this.fb.group({
+        kcal: 0,
+        proteins: 0,
+        carbs: 0,
+        fat: 0
+      })
+    });
   }
 
   createProduct(): void {
-    if (!!this.product) {
-      this.store.dispatch(ProductActions.postProduct({product: this.product}));
-    }
-    this.product = getEmptyProduct();
+    const product: Product = this.form.value;
+    this.store.dispatch(ProductActions.postProduct({product}));
   }
 }
