e4fcf42550668396348cd101782aa71bb5b0b930
Łukasz Osiński <nuras9988@gmail.com>
2019-09-09 21:46:02 +0200

    DIET-54 Initial price form removed.

--- src/app/modules/dashboard/modules/product/product-details/product-details-form.service.ts
+++ src/app/modules/dashboard/modules/product/product-details/product-details-form.service.ts
@@ -24,7 +24,7 @@ export class ProductDetailsFormService {
         carbs: [ undefined, Validators.required ],
         fat: [ undefined, Validators.required ]
       }),
-      prices: this.fb.array([ this.createPriceForm() ]),
+      prices: this.fb.array([]),
     });
   }
 
